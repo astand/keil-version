4523a615a417f976cf94ca3c6efb63e0cab454ed
astand <quater873@gmail.com>
2018-05-20 08:39:54 +0300

    Added some basic classes.

---
+++ mdkversion/Mdkversion.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mdkversion
+{
+    internal class Mdkversion
+    {
+
+        public string[] StatusMessages { get; set; }
+
+        public Mdkversion(List<string> ret)
+        {
+            vs = ret;
+
+            if (vs.Count() == 0)
+            {
+                innerfail = true;
+                StatusMessages = new string[] { "Please pass at least one parameter. \"mdkversion.exe ?\" will print instruction" };
+            }
+            else if (vs[0] == "?")
+            {
+                innerfail = true;
+                StatusMessages = HelpMessage;
+            }
+            else
+            {
+                StatusMessages = new string[] { "Nothing here " };
+            }
+        }
+
+        internal void UpdateVersion()
+        {
+            if (innerfail)
+                return;
+
+            CheckParameters();
+
+            if (GetCurrentConfigFile() == false)
+                CreateNewConfigFile();
+
+            UpdateConfigFileInfo();
+        }
+
+        private void UpdateConfigFileInfo()
+        {
+            if (innerfail == true) {return;}
+        }
+
+        private void CreateNewConfigFile()
+        {
+            if (innerfail == true) {return;}
+        }
+
+        private bool GetCurrentConfigFile()
+        {
+            if (innerfail == true) {return false;}
+
+            return false;
+        }
+
+        private void CheckParameters()
+        {
+            if (innerfail == true) {return;}
+        }
+
+        private readonly List<string> vs;
+
+        private string[] HelpMessage =
+        {
+            "This utility performs versioning control. Settings passed as parameters of commandline.",
+            "",
+            "",
+            "First parametr - path and name to config (.h) file",
+            "Next maybe up to 4 params in key=value format where:",
+            "  key - name of version number : [ma, mi, rev, bld]",
+            "  value - behavior : [none, inc, y, m, d, h]",
+            "  \"none\" - the number won't be changed",
+            "  \"inc\" - the number will be incremented after each build",
+            "  \"y\",\"m\",\"d\",\"h\" - the number will be equal to current year, month, day, hour",
+            "",
+            "  Example: mdkversion.exe \"sr/conf/conf.h\" ma=none mi=m rev=d bld=inc",
+            ""
+        };
+
+        private bool innerfail;
+    }
+}
--- mdkversion/Program.cs
+++ mdkversion/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,41 @@ namespace mdkversion
     {
         static void Main(string[] args)
         {
+            var ret = args.ToList();
+            //var ret = list.ToList();
+            var mdkver = new Mdkversion(ret);
+            mdkver.UpdateVersion();
+
+            foreach (var str in mdkver.StatusMessages)
+            {
+                Console.WriteLine(str);
+            }
+
+            var path = Directory.GetCurrentDirectory();
+
+            foreach (var item in ret)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine(path);
+            Console.WriteLine(path);
+            Console.WriteLine(path);
+            Console.WriteLine("Ended...");
         }
+
+        static string[] list = new string[]
+        {
+            "/conf/conf.h",
+            "major=null",
+            "minor=month",
+            "rev=day",
+            "build"
+        };
+
+        static string[] list1 = new string[]
+        {
+            "?"
+        };
     }
 }
